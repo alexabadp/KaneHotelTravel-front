9905271a7a308bcb91eeb7862a1e966fa37d53cd
Mariano Caceres <marianounlok@gmail.com>
2023-02-23 01:30:44 -0300

    DetailCity

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./views/Home/Home";
+import DetailCity from "./views/DetailCity/DetailCity";
 
-function App() {
-  console.log("hola")
+
+
+function App() { 
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home/:city" element={<DetailCity />} />
       </Routes>
     </div>
   );

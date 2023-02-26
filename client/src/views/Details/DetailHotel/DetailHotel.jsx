c8673392365da25e306bec77aec0f24525e90586
Carlos Waldo Rojas <krlosw9@gmail.com>
2023-02-26 16:20:55 -0500

    Feat: Creado el CheckoutForm entro del archivo DetailHotel.jsx

--- client/src/views/Details/DetailHotel/DetailHotel.jsx
+++ client/src/views/Details/DetailHotel/DetailHotel.jsx
@@ -2,6 +2,40 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getDetailHotel } from "../../../redux/actions";
+import {loadStripe} from '@stripe/stripe-js';
+import { Elements, CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import style from './CheckoutForm.module.css'
+
+const stripePromise = loadStripe(import.meta.env.VITE_KEY_STRIPE)
+
+const CheckoutForm = () =>{
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement)
+    });
+
+    if(!error){
+      console.log(paymentMethod);
+    }else{
+      console.log("Error: -> ",error);
+    }
+  }
+  
+  return (
+    <form onSubmit={handleSubmit} className={style.paymentForm}>
+      <CardElement />
+      <button>
+        Reservar
+      </button>
+    </form>
+  )
+}
 
 const DetailHotel = () => {
   const dispatch = useDispatch();
@@ -22,6 +56,9 @@ const DetailHotel = () => {
       <h4>{detailHotel.rating}</h4>
       <h4>{detailHotel.services}</h4>
       <h4>{detailHotel.description}</h4>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
     </div>
   );
 };

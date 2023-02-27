cbe5b55109c3213b0abcbb999557ebe4c1edaf47
alexabadp <aalex.abad96@gmail.com>
2023-02-26 23:08:23 -0500

    observaciones en la variables del archivo . env

--- client/src/views/Details/DetailHotel/DetailHotel.jsx
+++ client/src/views/Details/DetailHotel/DetailHotel.jsx
@@ -2,15 +2,18 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getDetailHotel } from "../../../redux/actions";
-import {loadStripe} from '@stripe/stripe-js';
+import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-import CheckoutForm from "../../../components/CheckoutForm/CheckoutForm"
+import CheckoutForm from "../../../components/CheckoutForm/CheckoutForm";
 
-const stripePromise = loadStripe(import.meta.env.VITE_KEY_STRIPE)
+// const stripePromise = loadStripe(import.meta.env.VITE_KEY_STRIPE)
+const stripePromise = loadStripe(
+  "pk_test_51MeUSYJo5kAZGuTWTiN6NsA5FRMyqId8smjQOgEObJw8rbCeHijt3N58dI0J5HfF48lROYvHLIzLE2QjAk8skODA00D3KU6iNb"
+);
 
 const DetailHotel = () => {
-  const amountHardCode = 10;//10usd
-  const descriptionHardCode = "Descripcion del pago";//10usd
+  const amountHardCode = 10; //10usd
+  const descriptionHardCode = "Descripcion del pago"; //10usd
   const dispatch = useDispatch();
   const params = useParams();
   const detailHotel = useSelector((state) => state.detailHotel);
@@ -30,7 +33,10 @@ const DetailHotel = () => {
       <h4>{detailHotel.services}</h4>
       <h4>{detailHotel.description}</h4>
       <Elements stripe={stripePromise}>
-        <CheckoutForm totalPayment={amountHardCode} descriptionPayment={descriptionHardCode}/>
+        <CheckoutForm
+          totalPayment={amountHardCode}
+          descriptionPayment={descriptionHardCode}
+        />
       </Elements>
     </div>
   );

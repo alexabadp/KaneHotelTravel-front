8ed1f8a4ff65d61f7979826bbd16129bbc963709
Mariano Caceres <marianounlok@gmail.com>
2023-02-26 21:22:35 -0300

    DetailBokking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from "react-router-dom";
 import Home from "./views/Home/Home";
-
 import axios from "axios";
 import DetailCity from "./views/Details/DetailCity/DetailCity";
 import DetailHotel from "./views/Details/DetailHotel/DetailHotel";
@@ -9,6 +9,7 @@ import DetailActivity from "./views/Details/DetailACtivity/DetailActivity";
 import Hotels from "./components/Hotel/Hotels";
 import Activities from "./components/Activity/Activities";
 import CreateHotel from "./views/Forms/CreateHotel/CreateHotel";
+import DetailBooking from "./views/Details/DetailBooking/DetailBooking";
 axios.defaults.baseURL = "http://localhost:3001";
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Route path="/:city/hotel/:hotel" element={<DetailHotel />} />
         <Route path="/:city/activity/:activity" element={<DetailActivity />} />
         <Route path="/:city/hotels" element={<Hotels />} />
+        <Route path="/detail/booking" element={<DetailBooking />} />
         <Route path="/:city/activities" element={<Activities />} />
         <Route path="/backoffice/hotel/create" element={<CreateHotel />} />
       </Routes>

ce451ed677c7784b3e9404d3f6518f6fc402fca0
Mariano Caceres <marianounlok@gmail.com>
2023-02-28 23:56:41 -0300

    detail con validaciones

--- client/src/views/Details/DetailBooking/DetailBooking.jsx
+++ client/src/views/Details/DetailBooking/DetailBooking.jsx
@@ -1,9 +1,11 @@
 import "react-datepicker/dist/react-datepicker.css";
 import "react-calendar/dist/Calendar.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
-import Select from "react-select"
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import Select from "react-select";
 import Calendar from "react-calendar";
 import dayjs from "dayjs";
 import React from "react";
@@ -13,9 +15,27 @@ import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import style from "./DetailBooking.module.css";
 import Modal from "react-bootstrap/Modal";
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "../../../components/CheckoutForm/CheckoutForm";
+
+function validate(data, dateValue) {
+  const errors = {};
+  if (!data.name) errors.name = "The Name is required";
+  if (!data.lastname) errors.lastname = "The Last Name id required";
+  if (!data.email) errors.email = "The E-mail is required";
+  const regex =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  if (!data.email || regex.test(data.email) === false)
+    errors.typeEmail = " / The type E-mail is invalid";
+  if (!data.phone) errors.phone = "The Phone is required";
+  if (data.phone.length > 10 || isNaN(data.phone))
+    errors.typePhone = "The type Phone is invalid";
+  if (!data.id) errors.id = "Id is required";
+  if (isNaN(data.id) || data.id.length > 8)
+    errors.idType = "Id type is invalid";
+  if (!data.rooms.length) errors.rooms = "Pleace select Room";
+  if (!dateValue[0]) errors.checkin = "Date Check-In is required";
+  return errors;
+}
 
 const DetailBooking = () => {
   const location = useLocation();
@@ -29,21 +49,56 @@ const DetailBooking = () => {
     name: "",
     lastname: "",
     email: "",
-    address: "",
-    city: "",
+    phone: 0,
     id: "",
     checkin: "",
     checkout: "",
     rooms: [],
     price: 0,
   });
+  0;
   // const stripePromise = loadStripe(import.meta.env.VITE_KEY_STRIPE)
   const stripePromise = loadStripe(
     "pk_test_51MeUSYJo5kAZGuTWTiN6NsA5FRMyqId8smjQOgEObJw8rbCeHijt3N58dI0J5HfF48lROYvHLIzLE2QjAk8skODA00D3KU6iNb"
   );
+  //Validacion
+  const [error, setError] = useState({});
+  const [botonActive, setActive] = useState(false);
+  useEffect(() => {
+    const validations = validate(data, dateValue);
+    setError(validations);
+  }, [data]);
+  useEffect(() => {
+    if (
+      !error.name &&
+      !error.lastname &&
+      !error.email &&
+      !error.typeEmail &&
+      !error.phone &&
+      !error.typePhone &&
+      !error.id &&
+      !error.idType &&
+      !error.rooms &&
+      !error.checkin
+    ) {
+      setActive(true);
+    } else {
+      setActive(false);
+    }
+  }, [error]);
+
+  //formato de fecha
   const formatDate = (date) => {
     return dayjs(date).format("DD/MM/YYYY");
   };
+  useEffect(() => {
+    let checkInDate = dayjs(dateValue[0]).format("DD/MM/YYYY");
+    let checkOutDate = dayjs(dateValue[1]).format("DD/MM/YYYY");
+    setData({ ...data, checkin: checkInDate, checkout: checkOutDate });
+  }, [dateValue]);
+  
+
+  //handler Functions
   const handlerInputName = (event) => {
     const input = event.target.name;
     const value = event.target.value;
@@ -59,11 +114,6 @@ const DetailBooking = () => {
     const value = event.target.value;
     setData({ ...data, [input]: value });
   };
-  const handlerInputAddress = (event) => {
-    const input = event.target.name;
-    const value = event.target.value;
-    setData({ ...data, [input]: value });
-  };
   const handlerInputCity = (event) => {
     const input = event.target.name;
     const value = event.target.value;
@@ -74,59 +124,50 @@ const DetailBooking = () => {
     const value = event.target.value;
     setData({ ...data, [input]: value });
   };
-  const handlerInputCheckIn = () => {
-    setData({ ...data, checkin: formatDate(dateValue[0]) });
-  };
-  const handlerInputCheckOut = () => {
-    setData({ ...data, checkout: formatDate(dateValue[1]) });
-  };
-  let totalPrice = 0
 
-  const datosRoom = location.state.rooms.map((e) => {
-    return {label: e.name, value: e.name}
-  })
+  let totalPrice = 0;
+
+  const datosRoom = location.state.rooms?.map((e) => {
+    return { label: e.name, value: e.name };
+  });
   const handlerOption = (event) => {
-    const roomName = event.value;    
+    const roomName = event.value;
     let newArray = data.rooms;
-    let find = newArray?.filter(e => e === roomName)
-    if(find.length > 0){
-      setData({...data})
+    let find = newArray?.filter((e) => e === roomName);
+    if (find.length > 0) {
+      setData({ ...data });
     } else {
-      setData({...data, rooms: [...data.rooms, roomName]})
+      setData({ ...data, rooms: [...data.rooms, roomName] });
     }
-    
   };
-//   function priceTotal(){
-//     const array = location.state.rooms
-//     const nameRoom = data.rooms
-//     for(let i = 0; i < nameRoom.length; i++){
-//       for(let obj of array){        
-//        if(obj.name == nameRoom[i]){
-//         return totalPrice = totalPrice + obj.price
-//        }     
-//     }
-//   }
-  
-// }
-// priceTotal()
- 
-  
-  
- 
+  //   function priceTotal(){
+  //     const array = location.state.rooms
+  //     const nameRoom = data.rooms
+  //     for(let i = 0; i < nameRoom.length; i++){
+  //       for(let obj of array){
+  //        if(obj.name == nameRoom[i]){
+  //         return totalPrice = totalPrice + obj.price
+  //        }
+  //     }
+  //   }
+
+  // }
+  // priceTotal()
+
   const handleDelete = (event) => {
     setData({
       ...data,
       rooms: data.rooms.slice().filter((e) => e !== event),
     });
   };
-  
+
   return (
     <div className={style.containerBookingGeneral}>
       <div className={style.detailBookingContainer}>
         <div>
           <Form>
             <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Group as={Col}>
                 <Form.Label className={style.titleBooking}>
                   First Name
                 </Form.Label>
@@ -137,8 +178,9 @@ const DetailBooking = () => {
                   placeholder="Your Name"
                 />
               </Form.Group>
+              {error.name && <span className={style.fail}>{error.name}</span>}
 
-              <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Group as={Col}>
                 <Form.Label className={style.titleBooking}>
                   Last Name
                 </Form.Label>
@@ -148,52 +190,64 @@ const DetailBooking = () => {
                   type="text"
                   placeholder="Your Last Name"
                 />
+                {error.lastname && (
+                  <span className={style.fail}>{error.lastname}</span>
+                )}
               </Form.Group>
             </Row>
 
-            <Form.Group className="mb-3" controlId="formGridAddress1">
+            <Form.Group className="mb-3" controlId="formGridEmail">
               <Form.Label className={style.titleBooking}>Your Email</Form.Label>
               <Form.Control
                 onChange={(e) => handlerInputEmail(e)}
                 name="email"
                 placeholder="user@email"
               />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formGridAddress2">
-              <Form.Label className={style.titleBooking}>Address</Form.Label>
-              <Form.Control
-                onChange={(e) => handlerInputAddress(e)}
-                name="address"
-                placeholder="Apartment, studio, or floor"
-              />
+              {error.email && <span className={style.fail}>{error.email}</span>}
+              {error.typeEmail && (
+                <span className={style.fail}>{error.typeEmail}</span>
+              )}
             </Form.Group>
 
             <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label className={style.titleBooking}>City</Form.Label>
+              <Form.Group as={Col}>
+                <Form.Label className={style.titleBooking}>Phone</Form.Label>
                 <Form.Control
+                  placeholder="Input Only Numbers Max: 10"
                   onChange={(e) => handlerInputCity(e)}
-                  name="city"
+                  name="phone"
                 />
+                {error.phone && (
+                  <span className={style.fail}>{error.phone}</span>
+                )}
+                {error.typePhone && (
+                  <span className={style.fail}>{error.typePhone}</span>
+                )}
               </Form.Group>
 
-              <Form.Group as={Col} controlId="formGridZip">
+              <Form.Group as={Col}>
                 <Form.Label className={style.titleBooking}>ID</Form.Label>
                 <Form.Control onChange={(e) => handlerInputId(e)} name="id" />
+                {error.id && <span className={style.fail}>{error.id}</span>}
+                {error.idType && (
+                  <span className={style.fail}>{error.idType}</span>
+                )}
               </Form.Group>
             </Row>
 
             <Form.Group as={Col} controlId="formGridState">
               <Form.Label className={style.titleBooking}>Rooms</Form.Label>
-              <Select 
+              <Select
                 options={datosRoom}
                 placeholder="Choice Room"
-                onChange={handlerOption}              
+                onChange={handlerOption}
               />
+              {error.rooms && <span className={style.fail}>{error.rooms}</span>}
             </Form.Group>
 
-            <Form.Label className={style.titleBooking}>Booking</Form.Label>
+            <Form.Label className={style.titleBooking}>
+              Check-In / Check-Out
+            </Form.Label>
             <div>
               <Calendar
                 minDate={new Date()}
@@ -201,6 +255,8 @@ const DetailBooking = () => {
                 onChange={(e) => setCheckIn(e)}
                 value={dateValue}
               />
+              {error.checkin && <span className={style.fail}>{error.checkin}</span>}
+              {error.checkout && <span className={style.fail}>{error.checkout}</span>}
             </div>
             <div>
               <div>
@@ -216,17 +272,20 @@ const DetailBooking = () => {
             </div>
             <div className={style.modal}>
               <>
-              <Link to={`/`}>
-                  <Button
-                    className={style.button}
-                    variant="primary"
-                    type="submit">
+                <Link to={`/`}>
+                  <Button className={style.button} variant="primary">
                     Cancel
                   </Button>
                 </Link>
-                <Button variant="primary" onClick={handleShow} className={style.button}>
-                  Detail Total
-                </Button>              
+                <Button
+                  id="btn-Summit"
+                  disabled={!botonActive}
+                  variant="primary"
+                  onClick={handleShow}
+                  className={style.button}
+                >
+                  Finalize Reservation
+                </Button>
                 <Modal show={show} onHide={handleClose}>
                   <Modal.Header closeButton>
                     <Modal.Title>Booking Summary</Modal.Title>
@@ -243,7 +302,7 @@ const DetailBooking = () => {
                       <p className={style.titleBooking}>ID: {data.id}</p>
                       <p className={style.titleBooking}>
                         Rooms:{" "}
-                        {data.rooms.map((e) => {
+                        {data.rooms?.map((e) => {
                           return <p key={e}>{e}</p>;
                         })}
                       </p>
@@ -251,6 +310,16 @@ const DetailBooking = () => {
                         Total Price: ${data.price}
                       </p>
                     </ul>
+                    <div className={style.stripe}>
+                      <Form.Group className="mb-3" controlId="formGridEmail">
+                        <Elements stripe={stripePromise}>
+                          <CheckoutForm
+                            totalPayment={amountHardCode}
+                            descriptionPayment={descriptionHardCode}
+                          />
+                        </Elements>
+                      </Form.Group>
+                    </div>
                   </Modal.Body>
                   <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
@@ -259,16 +328,6 @@ const DetailBooking = () => {
                   </Modal.Footer>
                 </Modal>
               </>
-            </div>
-            <div>
-              <Form className={style.stripe}>
-              <Elements stripe={stripePromise}>
-                <CheckoutForm
-                  totalPayment={amountHardCode}
-                  descriptionPayment={descriptionHardCode}
-                />
-              </Elements>
-              </Form>
             </div>
             {/* <Button className={style.button} variant="primary" type="submit">
               Submit
@@ -286,13 +345,13 @@ const DetailBooking = () => {
               return (
                 <div key={e}>
                   <div className={style.title1}>
-                  <button
-                  type="button"
-                  onClick={() => handleDelete(e)}
-                  className={style.btnDelete}
-                >
-                  X
-                </button>
+                    <button
+                      type="button"
+                      onClick={() => handleDelete(e)}
+                      className={style.btnDelete}
+                    >
+                      X
+                    </button>
                     <h2>{e}</h2>
                   </div>
                   {/* <div className={style.image}>
@@ -309,3 +368,26 @@ const DetailBooking = () => {
 };
 
 export default DetailBooking;
+
+{
+  /* <div>
+							<button
+								className={styles.containerButton}
+								type="submit"
+								disabled={
+									!input.name ||
+									!input.types.length > 0 ||
+									errors.name ||
+									errors.life ||
+									errors.attack ||
+									errors.defense ||
+									errors.speed ||
+									errors.height ||
+									errors.weight ||
+									errors.img ||
+									errors.types
+								}>
+								SUBMIT
+							</button>
+						</div> */
+}
